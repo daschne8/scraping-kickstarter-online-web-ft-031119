4cdaf905564b62ca3c5e30bb0a7db1bccd842c36
Daniel Schneider <daschne8@gmail.com>
2019-03-28 16:38:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,7 @@
-# require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/Kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
 end
